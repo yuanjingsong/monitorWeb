fb66ef7172b1642f45dbcc4dd1d10795fed232db
yuanjingsong <532473857@qq.com>
2019-03-07 10:56:28 +0800

    add ict and hust

--- monitor/monitor/spiders/Spider.py
+++ monitor/monitor/spiders/Spider.py
@@ -10,11 +10,20 @@ class Spider(scrapy.Spider):
     start_urls = []
     urls = []
     url_patterns= {}
+
     with open("./list") as file:
         line = file.readline()
         while(line):
-            url_patterns[line.split()[0]] = line.split()[1]
-            urls.append(line.split()[0])
+            url = line.split()[0]
+            first_pattern = line.split()[1]
+            second_pattern = line.split()[2]
+            print(second_pattern)
+            if url not in url_patterns:
+                url_patterns[url] = []
+
+            url_patterns[url].append(first_pattern)
+            url_patterns[url].append(second_pattern)
+            urls.append(url)
             line = file.readline()
 
     def start_requests(self):
@@ -26,11 +35,18 @@ class Spider(scrapy.Spider):
             url = response.url
             host = url.split("cn")[0]+ "cn"
             response = Selector(response)
-            news_lst = response.xpath(Spider.url_patterns[url])
+            news_lst = response.xpath(Spider.url_patterns[url][0])
+            print(news_lst.extract())
             for news in news_lst:
                 item = MonitorItem()
-                item['news_tiltle'] = news.xpath("a/@title").extract()
-                item['news_url'] = host + (news.xpath("a/@href").extract()[0].split("..")[1])
+                if (host == "http://www.ict.ac.cn"):
+                    print(Spider.url_patterns[url][1])
+                    item["news_tiltle"] = news.xpath(Spider.url_patterns[url][1] + "/text()").extract()
+                else:
+                    item['news_tiltle'] = news.xpath(Spider.url_patterns[url][1] + "/@title").extract()
+                
+                item['news_url'] = host + (news.xpath(Spider.url_patterns[url][1] + "/@href").extract()[0].split("..")[1])
+
                 print(item)
                 yield item
 
